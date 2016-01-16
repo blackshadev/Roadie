9c2f5d303c8cbade77bc700729bf3a0d080ead92
blackshadev <vinnie@script4web.nl>
2016-01-16 11:46:24 +0100

    Worked on a more universal way to read in streams to buffers, worked on the HttpResponse and working on HttpErrors

---
+++ BufferReader.ts
@@ -0,0 +1,52 @@
+﻿import { Readable } from 'stream'
+import { EventEmitter } from "events"
+
+enum ReaderState {
+    none,
+    reading,
+    done
+}
+
+export class BufferReader {
+    protected _stream: Readable;
+
+    private _buffer: Buffer;
+    get buffer(): Buffer { return this._buffer; }
+    get length(): number { return this._buffer.length; }
+    
+    private _iX: number = 0;
+
+    state: ReaderState = ReaderState.none;
+
+    constructor(contentLength: number, strm: Readable) {
+        contentLength = isNaN(contentLength) ? 0 : contentLength;
+        this._buffer = new Buffer(contentLength);
+
+        this._stream = strm;
+
+    }
+
+    read(cb: (data: Buffer) => void): void {
+
+        switch (this.state) {
+            case ReaderState.none:
+                this.state = ReaderState.reading;
+                this._stream.on("end", () => this.state = ReaderState.done);
+                this._stream.on("data", (chk: Buffer) => {
+                    const readSize = Math.min(chk.length, this._buffer.length - this._iX);
+                    chk.copy(this._buffer, this._iX, 0, readSize);
+                    this._iX += readSize;
+                });
+            case ReaderState.reading:
+                this._stream.on("end", () => cb(this._buffer));
+                break;
+            case ReaderState.done:
+                setTimeout(() => cb(this._buffer));
+                break;
+        }
+
+        
+    }
+
+
+}
--- http.ts
+++ http.ts
@@ -1,5 +1,8 @@
-﻿import { IncomingMessage, ServerResponse, Server as HttpServer, createServer as createHttpServer } from "http"
+﻿import { IncomingMessage, ServerResponse, Server as HttpServer, createServer as createHttpServer, STATUS_CODES } from "http"
 import { Server as HttpsServer, createServer as createHttpsServer } from "https"
+import { BufferReader } from "./BufferReader";
+
+import { errno, IError } from "./errno";
 
 export enum ReadingState {
     empty,
@@ -20,46 +23,86 @@ class HttpRequest {
     private _data: Buffer;
     protected readState: ReadingState = ReadingState.empty;
     readBody(cb: (data: Buffer) => void): void {
-        switch (this.readState) {
-            case ReadingState.read:
-                cb(this._data);
-                break;
-            case ReadingState.empty:
-                this.readState = ReadingState.reading;
-                let len = parseInt(this.header("content-length"));
-                this._data_len = isNaN(len) ? 0 : len;
-                this._data = new Buffer(this._data_len);
-                this._data_iX = 0;
-                this.req.on("data", (chk: Buffer) => {
-                    let len = Math.min(this._data_len - this._data_iX, chk.length);
-                    chk.copy(this._data, this._data_iX, 0, len);
-                    this._data_iX += len;
-                    this.req.on("end", () => this.readState = ReadingState.read);
-                });
-                // fall through
-            case ReadingState.reading:
-                this.req.on("end", () => cb(this._data));
-                break;
-        }
-
+        this.reader.read(cb);
     };
 
     protected req: IncomingMessage;
+    protected reader: BufferReader;
 
     constructor(req: IncomingMessage) {
         this.req = req;
-        this.readBody((data: Buffer) => {
-            console.log(data);
-        });
+        this.reader = new BufferReader(parseInt(this.header("content-length")), req);
+        
     }
 }
 
 class HttpResponse {
 
     protected resp: ServerResponse;
+    protected statusCode: number = 200;
+    protected headers: { [name: string]: string };
+
+    private eos: boolean = false;
+    protected _encoding: string = "utf8";
+    protected _data: Buffer | string;
+    protected _startTime: number;
 
     constructor(resp: ServerResponse) {
         this.resp = resp;
+        this.headers = {};
+        this._startTime = Date.now();
+    }
+
+    status(code: number): void {
+        this.statusCode = code;
+    }
+
+    header(headerName: string, value: string) : void {
+        this.headers[headerName] = value;
+    }
+
+    data(dat: Buffer|string|Object): void {
+        var bin = dat instanceof Buffer;
+        if (!bin && typeof (dat) === "object") {
+            dat = JSON.stringify(dat);
+            this.header("Content-Type", "application/json");
+        }
+
+        this._data = <Buffer|string>dat;
+    }
+
+    append(dat: Buffer | string): void {
+        var bin = dat instanceof Buffer;
+        if (!bin && typeof (dat) === "object")
+            dat = JSON.stringify(dat);
+
+        if (bin && this._data instanceof Buffer) Buffer.concat([<Buffer>this._data, <Buffer>dat]);
+        else this._data += dat.toString();
+    }
+
+    send(): void {
+        if (this.eos) return console.log("server", "Request already send");
+
+        var len = typeof (this._data) === "string" ? Buffer.byteLength(<string>this._data, this._encoding) : this._data.length;
+        this.headers["Content-Length"] = len + "";
+        this.headers["Date"] = new Date().toUTCString();
+
+        this.resp.writeHead(this.statusCode, this.headers);
+        this.resp.end(this._data);
+        this.eos = true;
+        
+        var t = Date.now() - this._startTime;
+        console.log("server", " send: " + typeof (this._data) +
+            " of length " + len + " bytes, took " + t + "ms");
+    }
+
+}
+
+
+export class HttpError {
+    static translateErrNo(no: string): IError { return errno[no]; };
+    static httpStatusText(no: string): string {
+        return STATUS_CODES[no];
     }
 
 }
@@ -68,9 +111,23 @@ export class HttpContext {
     request: HttpRequest;
     response: HttpResponse;
 
+    get url(): string { return this.request.url; } 
+    get method(): string { return this.request.method; } 
+
+
     constructor(req: IncomingMessage, resp: ServerResponse) {
         this.request = new HttpRequest(req);
         this.response = new HttpResponse(resp);
+
+        setTimeout(() => {
+            this.response.data({
+                testVal: true,
+                more: { less:true }
+            });
+            this.response.send();
+
+        })
+
     }
 }
 
